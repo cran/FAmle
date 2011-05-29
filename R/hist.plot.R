f57e3a41f3ddca750489f39dda0a3d5f0b44051a
Francois Aucoin <frank.aucoin@gmail.com>
2011-05-29 00:00:00 +0000

    version 1.2

--- R/hist.plot.R
+++ R/hist.plot.R
@@ -1,14 +1,9 @@
 hist.plot <-
 function(x,...)
 {
-	HIST <- hist(x$x.info[,'x'],plot=FALSE)
-	find.max <- NULL
-	try(find.max <- optimize(function(k) distr(k,x$dist,x$par.hat,'d'),interval=c(0,max(HIST$mids)),maximum=TRUE)$objective,silent=TRUE)
-	if(find.max > max(HIST$density) & !is.null(find.max))
-		hist(x$x.info[,'x'],freq=FALSE,col='gray',ylim=c(0,find.max*1.01),main='',xlab=x$data.name,border='white',cex.axis=.8)
-	else hist(x$x.info[,'x'],freq=FALSE,col='gray',main='',xlab=x$data.name,border='white',cex.axis=.8)
-	rug(x$x.info[,'x'],col='red',lwd=2)
+	hist(x$x.info[,'x'],freq=FALSE,col='gray',main='',xlab=x$data.name,border='white',cex.axis=.8)
 	fun <- function(a) distr(a,x$dist,x$par.hat)
+	rug(x$x.info[,'x'],col='red',lwd=2)
 	curve(fun,add=TRUE,col='red')
 }
 
